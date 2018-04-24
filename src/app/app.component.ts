10862ab6b82475a5161e10b0bd84961aaba2b4b9
ram1225 <geek.emp25@gmail.com>
2018-04-25 02:03:17 +0530

    fixed jquery-csv access issue, coded partially for line chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 //import * as $ from 'jquery';
-var $  = require('jquery');
-require('../../node_modules/jquery-csv/src/jquery.csv.js');
+require('../../node_modules/jquery/dist/jquery.min.js');
+var $ = require('../../node_modules/jquery-csv/src/jquery.csv.js');
 import { UploadCsvService } from '../web-services/uploadCsvService';
+
+
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,7 @@ import { UploadCsvService } from '../web-services/uploadCsvService';
 })
 export class AppComponent {
   data_list: any;
+
 
   constructor(private uploadCsvService: UploadCsvService) { }
 
@@ -27,19 +30,51 @@ export class AppComponent {
     reader.readAsText(file);
     reader.onloadend = (e) => {
       var csvData = reader.result;
-      var data = $.csv.toArrays(csvData); 
+      var jsonobject = this.CSV2JSON(csvData);
+      //  var data = $.toObjects(csvData);
+      //  var jsonobject = JSON.stringify(data);
+      console.log(jsonobject);
+      // if (data && data.length > 0) {
+      //   this.data_list = jsonobject;
+      //   console.log('Imported -' + jsonobject+ '- rows successfully!');
 
-      if (data && data.length > 0) {
-        this.data_list = data;
-        console.log('Imported -' + data.length + '- rows successfully!');
-
-      } else {
-        console.log('No data to import!');
-      }
+      // } else {
+      //   console.log('No data to import!');
+      // }
     };
 
     reader.onerror = () => {
       console.log('Unable to read ' + file);
     };
+  }
+
+  CSV2JSON(csv) {
+    var array = $.toArrays(csv);
+    var objArray = [];
+    for (var i = 0; i < array.length; i++) {
+      objArray[i] = {};
+      objArray[i].name = array[i][0];
+     
+      var seriesArray = [];
+      for (var k = 1; k < array[i].length; k++) {
+
+        seriesArray[k] = {};
+        seriesArray[k]['year'] = array[i][k].split('|')[0];
+        seriesArray[k]['score'] = array[i][k].split('|')[1];
+
+        //console.log(seriesArray[k]);
+        seriesArray.push(seriesArray[k]);
+      //  console.log(seriesArray[k]);
+        
+      }
+     // console.log("seriesArray" + seriesArray[k]);
+      objArray[i].series = seriesArray;
+
+    }
+
+    var json = JSON.stringify(objArray);
+    var str = json.replace(/},/g, "},\r\n");
+
+    return str;
   }
 }
